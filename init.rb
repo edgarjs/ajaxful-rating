cd2d97611e940c94e10a1ceeb155de8a60377906
Edgar J. Suarez <edgar.js@gmail.com>
2008-10-13 19:31:43 -0500

    starting a new branch...

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 require 'ajaxful_rating'
 require 'helpers/ajaxful_rating_helpers'
 
-ActiveRecord::Base.send(:include, Mimbles::AjaxfulRating)
-ActionView::Base.send(:include, Mimbles::AjaxfulRating::Helpers)
+ActiveRecord::Base.send(:include, AjaxfulRating)
+ActionView::Base.send(:include, AjaxfulRating::Helpers)
