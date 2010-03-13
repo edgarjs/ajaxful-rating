6708c2daff8b40297396d7f0b2a4494a171bb1bb
Denis Odorcic <denis.odorcic@gmail.com>
2010-03-13 12:22:21 +0800

    Remove updated to v2.2.x message

--- init.rb
+++ init.rb
@@ -1,6 +1,1 @@
-puts "------------------------------------------"
-puts "IMPORTANT: AjaxfulRating has been updated to v2.2.x and some options changed."\
-  "Please read the changelog at http://github.com/edgarjs/ajaxful-rating/blob/master/CHANGELOG"
-puts "------------------------------------------"
-
 require 'ajaxful_rating'
