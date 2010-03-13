47e7b0de05837067fa893fd02d74f7a8af56bde3
Denis Odorcic <denis.odorcic@gmail.com>
2010-03-12 23:13:27 -0500

    Remove Rails 2.x style generator

--- generators/ajaxful_rating/templates/model.rb
+++
@@ -1,6 +0,0 @@
-class Rate < ActiveRecord::Base
-  belongs_to :rater, :class_name => "<%= file_name.classify %>"
-  belongs_to :rateable, :polymorphic => true
-  
-  attr_accessible :rate, :dimension
-end
